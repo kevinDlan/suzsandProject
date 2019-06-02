bad0f2b8ec0577beaf627061af4cca144ce61579
kevinDlan <kevinkone19@gmail.com>
2019-06-02 11:32:47 +0100

    commande update

--- controller/selectionProduit.php
+++ controller/selectionProduit.php
@@ -1,17 +1,12 @@
 <?php
       session_start();
-      
+
       if (isset($_POST['id']))
       {
-        
-
         if (isset($_POST['id']))
         {
           $id = $_POST['id'];
-
         }
-        
-        
             try
             {
               $bdd = new PDO('mysql:host=localhost;dbname=suzsand_db;charset=utf8', 'root', 'dylan7729');
@@ -20,16 +15,9 @@
             {
               die('Erreur : '.$e->getMessage());
             }
-
-
             $req = $bdd->prepare('SELECT libelleMenu, descriptionMenu, photoMenu, prix FROM menu WHERE id = :id');
             $req->execute(array('id'=> $id));
-
-
             $resultat = $req->fetch();
-
-            
-
             $_SESSION['libelleMenu'] = $resultat['libelleMenu'];
             $_SESSION['descriptionMenu'] = $resultat['descriptionMenu'];
             $_SESSION['photoMenu'] = $resultat['photoMenu'];
@@ -37,8 +25,6 @@
 
             $req->closeCursor();
           header('Location: ../commande/commande.php');
-        
+
       }
  ?>
-
-
